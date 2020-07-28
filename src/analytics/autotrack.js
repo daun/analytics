84b80b58e80b4411ba0a22277da2100b5a83ce9d
Philipp Daun <post@philippdaun.net>
2020-07-28 19:06:15 +0200

    Move config into object

--- src/analytics/autotrack.js
+++ src/analytics/autotrack.js
@@ -9,27 +9,39 @@ import 'autotrack/lib/plugins/url-change-tracker';
 /* global ga */
 
 
-/**
- * The tracking ID for your Google Analytics property.
- * https://support.google.com/analytics/answer/1032385
- */
-const TRACKING_ID = 'UA-12345-1';
+const config = {
+  /**
+   * The tracking ID for your Google Analytics property.
+   * https://support.google.com/analytics/answer/1032385
+   */
+  trackingId: '',
 
+  /**
+   * Bump this when making backwards incompatible changes to the tracking
+   * implementation. This allows you to create a segment or view filter
+   * that isolates only data captured with the most recent tracking changes.
+   */
+  trackingVersion: '1',
 
-/**
- * Bump this when making backwards incompatible changes to the tracking
- * implementation. This allows you to create a segment or view filter
- * that isolates only data captured with the most recent tracking changes.
- */
-const TRACKING_VERSION = '1';
+  /**
+   * This setting can be used to more accurately predict session boundaries.
+   * Note: if your property contains views in several different time zones,
+   * do not use this setting.
+   */
+  timeZone: 'Europe/London',
 
+  /**
+   * A default value for dimensions so unset values always are reported as
+   * something. This is needed since Google Analytics will drop empty dimension
+   * values in reports.
+   */
+  nullValue: '(not set)',
 
-/**
- * A default value for dimensions so unset values always are reported as
- * something. This is needed since Google Analytics will drop empty dimension
- * values in reports.
- */
-const NULL_VALUE = '(not set)';
+  /**
+   * To anonymize the IP address for all hits, set this to true.
+   */
+  anonymizeIp: true,
+};
 
 
 /**
@@ -64,10 +76,15 @@ const metrics = {
 /**
  * Initializes all the analytics setup. Creates trackers and sets initial
  * values on the trackers.
+ *
+ * @param {Object=} opts
  */
-export const init = () => {
+export const init = (opts = {}) => {
   // Initialize the command queue in case analytics.js hasn't loaded yet.
   window.ga = window.ga || ((...args) => (ga.q = ga.q || []).push(args));
+
+  // Merge defaults and options
+  Object.assign(config, opts);
 
   createTracker();
   trackErrors();
@@ -102,8 +119,8 @@ export const trackError = (err = {}, fieldsObj = {}) => {
  * version fields. In non-production environments it also logs hits.
  */
 const createTracker = () => {
-  ga('create', TRACKING_ID, 'auto');
 
+  ga('create', config.trackingId, 'auto');
   // Ensures all hits are sent via `navigator.sendBeacon()`.
   ga('set', 'transport', 'beacon');
 };
@@ -149,13 +166,13 @@ const trackCustomDimensions = () => {
   // because Google Analytics will drop rows with empty dimension values
   // in your reports.
   Object.keys(dimensions).forEach((key) => {
-    ga('set', dimensions[key], NULL_VALUE);
+    ga('set', dimensions[key], config.nullValue);
   });
 
   // Adds tracking of dimensions known at page load time.
   ga((tracker) => {
     tracker.set({
-      [dimensions.TRACKING_VERSION]: TRACKING_VERSION,
+      [dimensions.TRACKING_VERSION]: config.trackingVersion,
       [dimensions.CLIENT_ID]: tracker.get('clientId'),
       [dimensions.WINDOW_ID]: uuid(),
     });
@@ -190,7 +207,7 @@ const requireAutotrackPlugins = () => {
   });
   ga('require', 'maxScrollTracker', {
     sessionTimeout: 30,
-    timeZone: 'America/Los_Angeles',
+    timeZone: config.timeZone,
     maxScrollMetricIndex: getDefinitionIndex(metrics.MAX_SCROLL_PERCENTAGE),
   });
   ga('require', 'outboundLinkTracker', {
@@ -200,8 +217,8 @@ const requireAutotrackPlugins = () => {
     sendInitialPageview: true,
     pageLoadsMetricIndex: getDefinitionIndex(metrics.PAGE_LOADS),
     visibleMetricIndex: getDefinitionIndex(metrics.PAGE_VISIBLE),
-    timeZone: 'America/Los_Angeles',
     fieldsObj: {[dimensions.HIT_SOURCE]: 'pageVisibilityTracker'},
+    timeZone: config.timeZone,
   });
   ga('require', 'urlChangeTracker', {
     fieldsObj: {[dimensions.HIT_SOURCE]: 'urlChangeTracker'},
@@ -240,7 +257,7 @@ const sendNavigationTimingMetrics = () => {
     ga('send', 'event', {
       eventCategory: 'Navigation Timing',
       eventAction: 'track',
-      eventLabel: NULL_VALUE,
+      eventLabel: config.nullValue,
       nonInteraction: true,
       [metrics.RESPONSE_END_TIME]: responseEnd,
       [metrics.DOM_LOAD_TIME]: domLoaded,
